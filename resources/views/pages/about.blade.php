f500ea9dc91c736b097d837878dcc6e1e4035d84
SANGO Omer <omersango777@gmail.com>
2021-08-06 18:12:01 +0000

    Add styling with Tailwind CSS

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -9,9 +9,13 @@
 @endsection -->
 
 @section('content')
-<img src="{{ asset('/images/wozaka.jpg') }}" alt="Logo WOZAKA">
+<img src="{{ asset('/images/wozaka.jpg') }}" alt="Logo WOZAKA" class="my-12 rounded-full shadow-md">
 
-	<p>Built with &hearts; by WOZAKA</p>
+	<h2 class="mb-5 text-gray-700">
+		Built with <span class="text-pink-500">&hearts;</span> by WOZAKA
+	</h2>
 
-	<p><a href="{{ route('home') }}">Revenir à la page d'accueil</a></p>
+	<p>
+		<a href="{{ route('home') }}" class="text-indigo-500 hover:text-indigo-600 underline">Revenir à la page d'accueil</a>
+	</p>
 @endsection
